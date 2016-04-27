500b4fe9f25545deb86542d468e43ed8bf209380
LightBlue Chang <avence12@gmail.com>
2016-04-27 16:40:49 +0000

    * Fix the issue of processing repeated msg. * fix the versions of dependency

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 var express    = require('express');        
 var https      = require('https');
 var request    = require('request');
@@ -19,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 var server = https.createServer(httpsOptions, app).listen(config.port, function(){
-    console.log('Secured API server listening on port ' + config.port);
+    console.log('Secured API server listening on port ' + config.port);    
 });
 
 // production error handler
@@ -32,6 +33,38 @@ app.use(function(err, req, res, next) {
     });
 });
 
+var genericTmpl = {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": [{
+                "title": "First card",
+                "subtitle": "Element #1 of an hscroll",
+                "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                "buttons": [{
+                    "type": "web_url",
+                    "url": "https://www.messenger.com",
+                    "title": "web url"
+                }, {
+                    "type": "postback",
+                    "title": "Postback",
+                    "payload": "Payload for first element in a generic bubble",
+                }],
+            }, {
+                "title": "Second card",
+                "subtitle": "Element #2 of an hscroll",
+                "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                "buttons": [{
+                    "type": "postback",
+                    "title": "Postback",
+                    "payload": "Payload for second element in a generic bubble",
+                }],
+            }]
+        }
+    }
+};
+
 // [Start of API Route]=============================================================================
 var router = express.Router();              // get an instance of the express Router
 router.get('/webhook/', function (req, res) {
@@ -41,24 +74,28 @@ router.get('/webhook/', function (req, res) {
     res.send('Error, wrong validation token');
 })
 
-router.post('/webhook/', function (req, res) {
-  console.log('entries: '+req.body.entry);
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      console.log('sender: ' + sender + '  text: ' + text);
-      if (text === 'Generic') {
-         sendGenericMessage(sender);
-         continue; // important! it prevents from processing the samt message repeatedly
-      }
-      // Handle a text message from this sender
-      //sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200));
+router.post('/webhook/', function (req, res) {      
+    var messaging_events = null;
+    if(undefined !== req.body.entry[0].messaging) {
+        messaging_events = req.body.entry[0].messaging;
+    } else {
+        res.send('entry is empty');
     }
-  }
-  res.status(200);
+    for (var i = 0 ; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
+        if (event.message && event.message.text) {
+            var text = event.message.text;
+            console.log('sender: ' + sender + '  text: ' + text);
+            if (text === 'Generic') {
+               sendGenericMessage(sender, genericTmpl);
+            }
+            // Handle a text message from this sender
+            sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200));
+        }
+        continue; // important! it prevents from processing the same message repeatedly
+    }
+    res.send(JSON.stringify({status: 'ok'}));
 });
 
 router.get('/msbot/', function (req, res) {
@@ -71,80 +108,43 @@ app.use('/' + config.fb.appName, router);
 // [End of API Route]=============================================================================
 
 function sendTextMessage(sender, text) {
-  messageData = {
-    text:text
-  }
-  request({
-    url: config.fb.msgUrl,
-    qs: {access_token:config.fb.pageToken},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
-
-// swallow error to prevent from crashing of nodemon
-function swallowError (error) {
-
-  // If you want details of the error in the console
-  console.log(error.toString());
-
-  this.emit('end');
-}
-
-function sendGenericMessage(sender) {
-    messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
+    var messageData = {
+       text:text
     }
     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
+        url: config.fb.msgUrl,
         qs: {access_token:config.fb.pageToken},
         method: 'POST',
         json: {
             recipient: {id:sender},
             message: messageData,
         }
+      }, function(error, response, body) {
+        if (error) {
+          console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+          console.log('Error: ', response.body.error);
+        }
+    });
+}
+
+function sendGenericMessage(sender, payload, cb) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:config.fb.pageToken},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: payload,
+        }
     }, function(error, response, body) {
         if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
+            console.log('Error sending messages: ', error);
+            return cb(err);
+        } else if (body.error) {
+            console.log('Error: ', body.error);
+            return cb(body.error);
         }
-    })
+        cb(null, body);
+    });
 }
